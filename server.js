44a9ea554b5a4b62afb678d0d2ec6c26253780f3
4thdraw <4thdraw2@gmail.com>
2022-03-17 16:04:16 +0900

    리액트설정완료

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ var app = express();
 
 var postsend = require('./api/postsend')
 var getsend = require('./api/getsend')
-var sqlsend = require('./api/introduce')
+var sqlreqres = require('./api/introduce')
 
 
 app.use('/postsend', postsend); 
 app.use('/getsend', getsend);
-app.use('/introduce', sqlsend);
+app.use('/introduce', sqlreqres);
 
 app.set('port',8080)
 app.get('/', (req, res) =>{  res.send(' / 루트요청 있었음'); })

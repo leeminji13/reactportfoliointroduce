745f1f269e7c21577ceb8f8f881bbfa6e1d7bb81
4thdraw <4thdraw2@gmail.com>
2022-03-31 13:20:52 +0900

    리액트요청접수 노드소스완성

--- api/preinterview.js
+++ api/preinterview.js
@@ -5,32 +5,34 @@ var normalpage = require('../routes/normal')
 var awssql = require('./awssql')
 
 router.use(express.urlencoded({ extended : true }))
-//리액트에서 비동기로 요청시
-router.get('/', (req, res, next) =>{   
-   var type = req.query.type; 
-   if( type == 'list'){  // localhost:3000/prointerview?type=list
-       // localhost:3000/prointerview?type=aws
-          req.body.mapper = "introduceSql" //mapper namespace로 설정
-          req.body.crud = "select" // select, insert, update, delete 중 선택
-          req.body.mapper_id = "interview" // sql문 정보를 담고있는 객체의 id
+
+router.post('/', (req, res, next) =>{   
+   var type = req.query.type;
+   req.body.mapper = "introduceSql";     
+  
+   if( type ){  
+      switch(type){
+
+         case 'list' : req.body.crud = "select"; 
+                       req.body.mapper_id = "interview";
+                       break;
+         case 'write': req.body.crud = "insert"; 
+                       req.body.mapper_id = "interviewInsert";
+                       break;
+         case 'modify': req.body.crud = "update"; 
+                       req.body.mapper_id = "interviewModify";
+                       break; 
+         default      : req.body.crud = "delete"; 
+                        req.body.mapper_id = "interviewDrop";
+                        break; 
+      }      
 
           router.use('/', awssql )
           next('route')
-       
-   }else if(type == 'write'){ // localhost:3000/prointerview?type=write
-        
-          req.body.mapper = "introduceSql" //mapper namespace로 설정
-          req.body.crud = "insert" // select, insert, update, delete 중 선택
-          req.body.mapper_id = "interviewInsert" // sql문 정보를 담고있는 객체의 id
-
-          router.use('/', awssql )
-          next('route')
-
    }
-   else{ //평범한 라우팅이 이쪽으로 오시오.
-        //localhost:3000/prointerview       
-        router.use('/', normalpage )
-        next('route')
+   else{           
+          router.use('/', normalpage )
+          next('route')
    }   
 })
 
